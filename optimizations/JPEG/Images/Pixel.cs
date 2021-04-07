a71532306b3dd1e97b457b1eeac0fe710d272f59
MaxCross <dan25001@gmail.com>
2021-04-07 22:45:43 +0500

    Переделал Pixel в структуру с 3 цветовыми компонентами

--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace JPEG.Images
 {
-    public class Pixel
+    public struct Pixel
     {
         private readonly PixelFormat format;
 
@@ -12,34 +12,21 @@ namespace JPEG.Images
             if (!new[]{PixelFormat.RGB, PixelFormat.YCbCr}.Contains(pixelFormat))
                 throw new FormatException("Unknown pixel format: " + pixelFormat);
             format = pixelFormat;
-            if (pixelFormat == PixelFormat.RGB)
-            {
-                r = firstComponent;
-                g = secondComponent;
-                b = thirdComponent;
-            }
-            if (pixelFormat == PixelFormat.YCbCr)
-            {
-                y = firstComponent;
-                cb = secondComponent;
-                cr = thirdComponent;
-            }
+            c0 = firstComponent;
+            c1 = secondComponent;
+            c2 = thirdComponent;
         }
 
-        private readonly double r;
-        private readonly double g;
-        private readonly double b;
+        private readonly double c0;
+        private readonly double c1;
+        private readonly double c2;
 
-        private readonly double y;
-        private readonly double cb;
-        private readonly double cr;
+        public double R => format == PixelFormat.RGB ? c0 : (298.082 * c0 + 408.583 * c2) / 256.0 - 222.921;
+        public double G => format == PixelFormat.RGB ? c1 : (298.082 * c0 - 100.291 * c1 - 208.120 * c2) / 256.0 + 135.576;
+        public double B => format == PixelFormat.RGB ? c2 : (298.082 * c0 + 516.412 * c1) / 256.0 - 276.836;
 
-        public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
-        public double G => format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-        public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
-
-        public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-        public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-        public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+        public double Y => format == PixelFormat.YCbCr ? c0 : 16.0 + (65.738 * c0 + 129.057 * c1 + 24.064 * c2) / 256.0;
+        public double Cb => format == PixelFormat.YCbCr ? c1 : 128.0 + (-37.945 * c0 - 74.494 * c1 + 112.439 * c2) / 256.0;
+        public double Cr => format == PixelFormat.YCbCr ? c2 : 128.0 + (112.439 * c0 - 94.154 * c1 - 18.285 * c2) / 256.0;
     }
 }
