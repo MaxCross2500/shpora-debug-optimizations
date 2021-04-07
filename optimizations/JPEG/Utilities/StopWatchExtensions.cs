e943bee68fd7ae4bb302552d86fdd651546a9bd8
MaxCross <dan25001@gmail.com>
2021-04-07 15:51:21 +0500

    Добавил StopWatchExtensions

---
+++ optimizations/JPEG/Utilities/StopWatchExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace JPEG.Utilities
+{
+	public static class StopWatchExtensions
+	{
+		public static void ReportAndRestart(this Stopwatch stopwatch, string text)
+		{
+			stopwatch.Stop();
+			Console.WriteLine($"{text}: {stopwatch.Elapsed}");
+			stopwatch.Restart();
+		}
+		
+		public static void ReportAndRestart(this Stopwatch stopwatch, string text, params object[] args)
+		{
+			stopwatch.Stop();
+			Console.WriteLine(text, args.Prepend(stopwatch.Elapsed).ToArray());
+			stopwatch.Restart();
+		}
+	}
+}
